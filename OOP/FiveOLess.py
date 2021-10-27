d49e0ff9ee24810ce2ed3fc961ccf343939b85ab
yusufadell <youseefadel777@gmail.com>
2021-10-28 01:12:09 +0200

    Add FiveOLess With List comp solution

--- OOP/FiveOLess.py
+++ OOP/FiveOLess.py
@@ -15,24 +15,32 @@ Author: yusufadell
 
 userinput = input("Enter numbers seperated by space: ")
 
-try:
-    tmp = userinput.split(" ")
-    # List = [int(i) for i in tmp] Using list comprehension
-    List = []
-    for i in tmp:
-        List.append(int(i))
 
-    # ans = [i for i in List if i < 5]
-    ans = []
-    for i in List:
-        if i < 5:
-            ans.append(i)
+# Ambiguous Way Using list comprehension and not recommended!.
+tmp = userinput.split(" ")
+List = [int(i) for i in tmp]
+ans = [i for i in List if i < 5]
 
-    # The function sorted(): sort the list before print it to the user
-    if len(ans) == 0:
-        print(f"There is no numbers less than 5 in {sorted(List)}")
-    else:
-        print(f"{sorted(ans)} numbers less than 5 in {sorted(List)}")
+print(ans)
 
-except Exception as e:
-    print("Enter a valid Integer!")
+# try:
+#     tmp = userinput.split(" ")
+#     List = [int(i) for i in tmp] Using list comprehension
+#     List = []
+#     for i in tmp:
+#         List.append(int(i))
+
+#     ans = [i for i in List if i < 5]
+#     ans = []
+#     for i in List:
+#         if i < 5:
+#             ans.append(i)
+
+#     # The function sorted(): sort the list before print it to the user
+#     if len(ans) == 0:
+#         print(f"There is no numbers less than 5 in {sorted(List)}")
+#     else:
+#         print(f"{sorted(ans)} numbers less than 5 in {sorted(List)}")
+
+# except Exception as e:
+#     print("Enter a valid Integer!")
